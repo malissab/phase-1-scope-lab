938ec26adf85f90ccf47758af69f9c56c61292ff
Malissa Bishop <malissambishop7@gmail.com>
2021-10-13 23:02:26 -0700

    commit tree e7070e2727ec64e8b80eb2b58f9a4df9a636fb19

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'rude ppl'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'customers who dont tip'
+}
